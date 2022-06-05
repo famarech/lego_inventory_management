528c835877f8ef96e275db08a69593cd6312465f
famarech <marechalfabrice@hotmail.fr>
2022-06-05 15:22:09 +0200

    V2.1 ajout fonction d'import pour des sets depuis Blk format txt et xml

--- ReadModule.py
+++ ReadModule.py
@@ -9,6 +9,61 @@ file_xml = abspath('./ressources/exemples/download_inv_store_from_blk_xml_format
 file_csv_by_me = abspath('essai.csv')
 file_csv_from_blk = abspath('./ressources/exemples/download_inv_store_from_blk.csv')
 file_txt_from_blk = abspath('./ressources/exemples/download_inv_store_from_blk_tab_delimited.txt')
+file_set_xml = abspath('./ressources/exemples/set_from_blk.xml')
+file_set_txt = abspath('./ressources/exemples/set_from_blk.txt')
+
+
+
+
+
+def from_txt_set(file):
+    tab = []
+    with open(file, newline='') as csvfile:
+        data = csv.reader(csvfile.readlines()[2:], delimiter='\t')
+        for row in data:
+            # row[6] est ALTERNATE, faut-il le décompter ?
+            if row[8] == "N" and row[5] == "N" and row[0] == "P":
+                tab.append(ITEM('', '', '', row[4], '',
+                                row[3], '', '', '', '',
+                                row[0], row[1], '', '', '',
+                                '', '', '', '', '',
+                                row[2], '', '', '', '',
+                                '', '', ''))
+    return tab
+
+
+
+def from_xml_set(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    tab = []
+    for item in root:
+        # item[5].text est ALTERNATE, faut-il le décompter ?
+        if item[7].text == "N" and item[4].text == "N" and item[0].text == "P":
+            tab.append(ITEM('', '', '', item[3].text, '',
+                            item[2].text, '', '', '', '',
+                            item[0].text, item[1].text, '', '', '',
+                            '', '', '', '', '',
+                            '', '', '', '', '',
+                            '', '', ''))
+    return tab
+
+
+
+set = from_txt_set(file_set_txt)
+for each in set:
+    each.afficher()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -37,7 +92,6 @@ def get_category(laurel, hardy, value, file):
 
 def from_txt_from_blk(file):
     tab = []
-    index = 0
     with open(file, newline='') as csvfile:
         data = csv.reader(csvfile, delimiter='\t')
         try:
@@ -61,7 +115,6 @@ def from_txt_from_blk(file):
 
 def from_csv_from_blk(file):
     tab = []
-    index = 0
     with open(file, newline='') as csvfile:
         data = csv.reader(csvfile, delimiter=',')
         try:
@@ -105,7 +158,7 @@ def from_xml_v2(file):
 def from_csv_by_me(file):
     tab = []
     with open(file, newline='') as csvfile:
-        data = csv.reader(csvfile, delimiter=';', quotechar=' ')
+        data = csv.reader(csvfile.readlines()[:1], delimiter=';', quotechar=' ')
         for row in data:
             tab.append(ITEM(row[0], row[1], row[2], row[3], row[4],
                                 row[5], row[6], row[7], row[8], row[9],
@@ -113,7 +166,6 @@ def from_csv_by_me(file):
                                 row[15], row[16], row[17], row[18], row[19],
                                 row[10], row[21], row[22], row[23], row[24],
                                 row[25], row[26], row[27]))
-    del tab[0]
     return tab
 
 
